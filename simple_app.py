015673b2d38501a0f483d5eafddc5cb0c62343d0
solvaring <brandon.wall0711@gmail.com>
2015-11-13 01:36:15 -0800

    Finished Welcome To Flask lesson1 flask basics

---
+++ simple_app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<name>')
+def index(name="Solvaring"):
+    return "Hello from {}".format(name)
+
+
+
+@app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+def add(num1, num2):
+    return '{} + {} = {}'.format(num1, num2, num1+num2)
+
+if __name__ == "__main__":
+    from os import environ
+    if 'WINGDB_ACTIVE' not in environ:
+        app.debug = True
+    app.run(port=8000, host='localhost', use_reloader=True)    
