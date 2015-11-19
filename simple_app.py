9db12f7542235eb8554ef8fd4d3a8fec1824bb58
solvaring <brandon.wall0711@gmail.com>
2015-11-18 23:40:17 -0800

    completion of basic flask page

--- simple_app.py
+++ simple_app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 # from flask import request
 
 app = Flask(__name__)
@@ -7,7 +8,7 @@ app = Flask(__name__)
 @app.route('/<name>')
 def index(name="Solvaring"):
     # name = request.args.get('name', name) 
-    return "Hello from {}".format(name)
+    return render_template("index.html", name=name)
 
 
 
@@ -16,10 +17,11 @@ def index(name="Solvaring"):
 @app.route('/add/<float:num1>/<int:num2>')
 @app.route('/add/<int:num1>/<float:num2>')
 def add(num1, num2):
-    return '{} + {} = {}'.format(num1, num2, num1+num2)
+    context = {'num1': num1, 'num2': num2}
+    return render_template("add.html", **context)
 
 if __name__ == "__main__":
     from os import environ
     if 'WINGDB_ACTIVE' not in environ:
         app.debug = True
-    app.run(port=8000, host='localhost', use_reloader=True)    
+    app.run(port=8000, host='192.168.1.114', use_reloader=True)    
